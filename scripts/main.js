4fcda19a84ed33dcea44957078f7ca4e91a4a2a0
Rachel Dixon <ms.racheldixon@gmail.com>
2023-11-29 19:48:23 -0500

    play with event listener for cards

--- scripts/main.js
+++ scripts/main.js
@@ -10,15 +10,16 @@ MicroModal.init({
     
 // console.log("yo");
 // // find element for event to be on
-// var cardsBackground = document.querySelector(".cards");
-// var cards = document.querySelectorAll(".card");
-// //var backgroundImages = ["/memetic-tarot/assets/img/Cups02.jpg", "/false-idols/runeroom/assets/img/bg3.jpg", "/false-idols/runeroom/assets/img/bg4.jpg", "/false-idols/runeroom/assets/img/bg5.jpg", "/false-idols/runeroom/assets/img/bg8.jpg", "/false-idols/runeroom/assets/img/bg6.jpg", "/false-idols/runeroom/assets/img/bg7.jpg", "/false-idols/runeroom/assets/img/bg9.jpg", "/false-idols/runeroom/assets/img/bg10.jpg"];
+var cardsBackground = document.querySelector(".cards");
+var cards = document.querySelectorAll(".card");
+var backgroundImages = ["/memetic-tarot/assets/img/Cups02.jpg", "../assets/img/TarokkKartya_Barely_09Cup.jpg", "../assets/img/Three_of_Cups\,_Waite-Smith_Tarot_Deck\,_Yale_University.jpg", "../assets/img/ChaosOracleDuDames.jpg", "../assets/img/Sola_Busca_Cups02.jpg", "../assets/img/MinchiateCups04.jpg", "../assets/img/Etteilla-QueenCups.jpeg", "", "../assets/img/Bembo-Visconti-KnaveOfCups.jpg"];
+
 // // add event listener with Math random loop
-// cards.forEach(card => {
-//   card.addEventListener('click', function() {
-//     var randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
-//     cardsBackground.style.backgroundImage = `url(${randomImage})`;
+cards.forEach(card => {
+  card.addEventListener('click', function() {
+    var randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
+    cardsBackground.style.backgroundImage = `url(${randomImage})`;
     
-  // console.log("6+9") ;
-  // });
-// });
+  console.log("6+9") ;
+  });
+});
