40673ec1a7f38369b88b3847fd6657aa315aa5ed
Rachel Dixon <ms.racheldixon@gmail.com>
2023-12-11 14:01:46 -0500

    added new CardDeck shuffle algo (partial)

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,12 @@ MicroModal.init({
   });
     
 // console.log("yo");
-// // find element for event to be on
+// find element for event to be on
 var cardsBackground = document.querySelector(".cards");
 var cards = document.querySelectorAll(".card");
 var backgroundImages = ["/memetic-tarot/assets/img/Cups02.jpg", "../assets/img/TarokkKartya_Barely_09Cup.jpg", "../assets/img/Three_of_Cups\,_Waite-Smith_Tarot_Deck\,_Yale_University.jpg", "../assets/img/ChaosOracleDuDames.jpg", "../assets/img/Sola_Busca_Cups02.jpg", "../assets/img/MinchiateCups04.jpg", "../assets/img/Etteilla-QueenCups.jpeg", "", "../assets/img/Bembo-Visconti-KnaveOfCups.jpg"];
 
-// // add event listener with Math random loop
+// add event listener with Math random loop
 cards.forEach(card => {
   card.addEventListener('click', function() {
     var randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
@@ -23,3 +23,10 @@ cards.forEach(card => {
   console.log("6+9") ;
   });
 });
+// try shuffle on card "deck" rather than background images
+// function shuffleDeck(deck) {
+//   for (let i = deck.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1));
+//     [deck[i], deck[j]] = [deck[j], deck[i]];
+//   }
+// }
