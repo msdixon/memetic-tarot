6730351e60c3461b218fddce8220ef6a090a545b
Rachel Dixon <ms.racheldixon@gmail.com>
2023-12-12 20:50:07 -0500

    comment out the entire algo thingamabob, fix js directory link in html

--- scripts/main.js
+++ scripts/main.js
@@ -14,14 +14,14 @@ var cards = document.querySelectorAll(".card");
 var backgroundImages = ["/memetic-tarot/assets/img/Cups02.jpg", "../assets/img/TarokkKartya_Barely_09Cup.jpg", "../assets/img/Three_of_Cups\,_Waite-Smith_Tarot_Deck\,_Yale_University.jpg", "../assets/img/ChaosOracleDuDames.jpg", "../assets/img/Sola_Busca_Cups02.jpg", "../assets/img/MinchiateCups04.jpg", "../assets/img/Etteilla-QueenCups.jpeg", "", "../assets/img/Bembo-Visconti-KnaveOfCups.jpg"];
 
 // add event listener with Math random loop
-cards.forEach(card => {
-  card.addEventListener('click', function() {
-    var randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
+// cards.forEach(card => {
+//   card.addEventListener('click', function() {
+//     var randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
     // cardsBackground.style.backgroundImage = `url(${randomImage})`;
     
   console.log("6+9") ;
-  });
-});
+//   });
+// });
 // try shuffle on card "deck" rather than background images
 // function shuffleDeck(deck) {
 //   for (let i = deck.length - 1; i > 0; i--) {
