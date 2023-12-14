cb85a472d88108b2a7f1fb864fe5d5261e222e75
Rachel Dixon <ms.racheldixon@gmail.com>
2023-12-14 10:32:51 -0500

    modal button z-axis fixes, fixed loading console messages & comment out shuffle

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,18 @@
 
-document.addEventListener("DOMContentLoaded", function(){console.log('loaded') });
+document.addEventListener("DOMContentLoaded", function(){console.log('DOMloaded') });
 
 
-console.log(5 + 6);
 
 MicroModal.init({
     disableScroll: true
   });
-    
+
+
+  console.log("modalsLoaded");
 // find element for event to be on
-var cardsBackground = document.querySelector(".cards");
-var cards = document.querySelectorAll(".card");
-var backgroundImages = ["/memetic-tarot/assets/img/Cups02.jpg", "../assets/img/TarokkKartya_Barely_09Cup.jpg", "../assets/img/Three_of_Cups\,_Waite-Smith_Tarot_Deck\,_Yale_University.jpg", "../assets/img/ChaosOracleDuDames.jpg", "../assets/img/Sola_Busca_Cups02.jpg", "../assets/img/MinchiateCups04.jpg", "../assets/img/Etteilla-QueenCups.jpeg", "", "../assets/img/Bembo-Visconti-KnaveOfCups.jpg"];
+// var cardsBackground = document.querySelector(".cards");
+// var cards = document.querySelectorAll(".card");
+// var backgroundImages = ["/memetic-tarot/assets/img/Cups02.jpg", "../assets/img/TarokkKartya_Barely_09Cup.jpg", "../assets/img/Three_of_Cups\,_Waite-Smith_Tarot_Deck\,_Yale_University.jpg", "../assets/img/ChaosOracleDuDames.jpg", "../assets/img/Sola_Busca_Cups02.jpg", "../assets/img/MinchiateCups04.jpg", "../assets/img/Etteilla-QueenCups.jpeg", "", "../assets/img/Bembo-Visconti-KnaveOfCups.jpg"];
 
 // add event listener with Math random loop
 // cards.forEach(card => {
@@ -19,7 +20,7 @@ var backgroundImages = ["/memetic-tarot/assets/img/Cups02.jpg", "../assets/img/T
 //     var randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
     // cardsBackground.style.backgroundImage = `url(${randomImage})`;
     
-  console.log("6+9") ;
+  console.log("event listener") ;
 //   });
 // });
 // try shuffle on card "deck" rather than background images
