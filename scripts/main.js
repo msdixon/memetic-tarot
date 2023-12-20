14c270cbac821573044939c74aa70536a487ae36
Rachel Dixon <ms.racheldixon@gmail.com>
2023-12-20 11:46:51 -0500

    additional styling changes - name changes to JS

--- scripts/main.js
+++ scripts/main.js
@@ -10,14 +10,14 @@ MicroModal.init({
 
   console.log("modalsLoaded");
 // find element for event to be on
-var cardsBackground = document.querySelector(".cards");
+var deck = document.querySelector(".cards");
 var cards = document.querySelectorAll(".card");
-var backgroundImages = ["/memetic-tarot/assets/img/Cups02.jpg", "../assets/img/TarokkKartya_Barely_09Cup.jpg", "../assets/img/Three_of_Cups\,_Waite-Smith_Tarot_Deck\,_Yale_University.jpg", "../assets/img/ChaosOracleDuDames.jpg", "../assets/img/Sola_Busca_Cups02.jpg", "../assets/img/MinchiateCups04.jpg", "../assets/img/Etteilla-QueenCups.jpeg", "", "../assets/img/Bembo-Visconti-KnaveOfCups.jpg"];
+var tarotImages = ["/memetic-tarot/assets/img/Cups02.jpg", "../assets/img/TarokkKartya_Barely_09Cup.jpg", "../assets/img/Three_of_Cups\,_Waite-Smith_Tarot_Deck\,_Yale_University.jpg", "../assets/img/ChaosOracleDuDames.jpg", "../assets/img/Sola_Busca_Cups02.jpg", "../assets/img/MinchiateCups04.jpg", "../assets/img/Etteilla-QueenCups.jpeg", "", "../assets/img/Bembo-Visconti-KnaveOfCups.jpg"];
 
 // add event listener with Math random loop
 cards.forEach(card => {
   card.addEventListener('click', function() {
-    var randomImage = backgroundImages[Math.floor(Math.random() * backgroundImages.length)];
+    var randomImage = tarotImages[Math.floor(Math.random() * tarotCards.length)];
     cardsBackground.style.backgroundImage = `url(${randomImage})`;
   });  
  });
