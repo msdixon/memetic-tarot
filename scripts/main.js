4726327ea220cd159de5674eb0e877bf4dab6d77
Rachel Dixon <ms.racheldixon@gmail.com>
2023-12-22 20:20:25 -0500

    imgs to html bc thats ze rules/make shuffle happen

--- scripts/main.js
+++ scripts/main.js
@@ -10,15 +10,15 @@ MicroModal.init({
 
   console.log("modalsLoaded");
 // find element for event to be on
-var deck = document.querySelector(".cards");
-var cards = document.querySelectorAll(".card");
+var card = document.querySelectorAll(".card");
+var shuffle = document.querySelector(".shuffle-button");
 var tarotImages = ["/memetic-tarot/assets/img/Cups02.jpg", "../assets/img/TarokkKartya_Barely_09Cup.jpg", "../assets/img/Three_of_Cups\,_Waite-Smith_Tarot_Deck\,_Yale_University.jpg", "../assets/img/ChaosOracleDuDames.jpg", "../assets/img/Sola_Busca_Cups02.jpg", "../assets/img/MinchiateCups04.jpg", "../assets/img/Etteilla-QueenCups.jpeg", "", "../assets/img/Bembo-Visconti-KnaveOfCups.jpg"];
 
 // add event listener with Math random loop
-cards.forEach(card => {
-  card.addEventListener('click', function() {
-    var randomImage = tarotImages[Math.floor(Math.random() * tarotCards.length)];
-    cardsBackground.style.backgroundImage = `url(${randomImage})`;
+card.forEach(shuffle => {
+  shuffle.addEventListener('click', function() {
+    var randomImage = tarotImages[Math.floor(Math.random() * tarotImages.length)];
+    tarotImages.style.card = `url(${randomImage})`;
   });  
  });
   console.log("event listener") ;
