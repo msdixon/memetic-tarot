1bf025e61bd108db7c384016a7b753cfc10f3f5f
Rachel Dixon <ms.racheldixon@gmail.com>
2024-01-05 13:29:50 -0500

    add alt text to tarotImages array

--- scripts/main.js
+++ scripts/main.js
@@ -12,8 +12,29 @@ console.log("modalsLoaded");
 // declare card containers and card image variables 
 let cardImages = document.querySelectorAll(".card_img");
 // create array of tarot images for the cards to shuffle through
-let tarotImages = ["assets/img/AceCoups1760.jpeg", "assets/img/06BolonaisAllaTorre.jpeg", "assets/img/AceCupsVSf.jpeg", "assets/img/09CupsVsf.jpeg", "assets/img/Sola_Busca_Cups10.jpeg", "assets/img/KingCupsPiedmontese.jpg", "assets/img/AceOfCupsFlickr.jpg", "assets/img/ThreeCupsRWS.jpg", "assets/img/TarokkKartya_Barely_09Cup.jpg", "assets/img/Bembo-Visconti-KnaveOfCups.jpg", "assets/img/Sola_Busca_Cups06.jpeg", "assets/img/ChaosOracleDuDames.jpg", "assets/img/Sola_Busca_Cups02.jpg", "assets/img/MinchiateCups04.jpg", "assets/img/Etteilla-QueenCups.jpeg", "assets/img/FiveCupsRWS.jpg", "assets/img/FrenchTarotWedding.jpeg", "assets/img/Cups02.jpg", "assets/img/Minchiate03Cups.jpeg"];
-
+// let tarotImages = ["assets/img/AceCoups1760.jpeg", "assets/img/06BolonaisAllaTorre.jpeg", "assets/img/AceCupsVSf.jpeg", "assets/img/09CupsVsf.jpeg", "assets/img/Sola_Busca_Cups10.jpeg", "assets/img/KingCupsPiedmontese.jpg", "assets/img/AceOfCupsFlickr.jpg", "assets/img/ThreeCupsRWS.jpg", "assets/img/TarokkKartya_Barely_09Cup.jpg", "assets/img/Bembo-Visconti-KnaveOfCups.jpg", "assets/img/Sola_Busca_Cups06.jpeg", "assets/img/ChaosOracleDuDames.jpg", "assets/img/Sola_Busca_Cups02.jpg", "assets/img/MinchiateCups04.jpg", "assets/img/Etteilla-QueenCups.jpeg", "assets/img/FiveCupsRWS.jpg", "assets/img/FrenchTarotWedding.jpeg", "assets/img/Cups02.jpg", "assets/img/Minchiate03Cups.jpeg"];
+let tarotImages = [
+  {src: "assets/img/AceCoups1760.jpeg", alt: "Ace de Coups, an 18th century tarot card", cardNumberValue: "1", suit: "Cups", modal: "modal1" },
+  {src: "assets/img/AceOfCupsFlickr.jpg", alt: "Ace of Cups from Rider-Waite-Smith tarot deck, two interlocking standing figures hold cups to the sky", cardNumberValue: "1", suit: "Cups", modal: "modal2" },
+  {src: "assets/img/06BolonaisAllaTorre.jpeg", alt: "Six of Cups, from the Bolonais Alla Torre tarot deck" },
+  {src: "assets/img/AceCupsVSf.jpeg", alt: "Ace of Cups from the Vincenti-Sforza tarot deck, one gold cup filled with a gold baton with an angel figure at the top" },
+  {src: "assets/img/09CupsVsf.jpeg", alt:  "Nine of Cups from the Vincenti-Sforza tarot deck"}, 
+  {src: "assets/img/Sola_Busca_Cups10.jpeg", alt: "Ten of Cups from the Sola Busca tarot deck"},
+  {src: "assets/img/KingCupsPiedmontese.jpg", alt: "King of Cups from the Piedmontese tarot deck"}, 
+  {src: "assets/img/AceOfCupsFlickr.jpg", alt: "Ace of Cups from Rider-Waite-Smith tarot deck, with one large cup overflowing with golden substance"}, 
+  {src: "assets/img/ThreeCupsRWS.jpg", alt: "Three of Cups from the Rider-Waite-Smith tarot deck, with three robed figures holding cups up to the sky facing each other"}, 
+  {src: "assets/img/TarokkKartya_Barely_09Cup.jpg", alt: "Nine of Cups from the Tarokk Kartya card deck, with a reflected figure of a couple dancing outside, and on the reflection a couple in a field with an animal, one person drinking from a cup and the other selecting items from a fruit cart"}, 
+  {src: "assets/img/Bembo-Visconti-KnaveOfCups.jpg", alt: "Page of Cups from the 15th century Bembo-Visconti tarot deck, with a figure of a young person in regal dress standing and holding a cup with arm outstretched" }, 
+  {src: "assets/img/Sola_Busca_Cups06.jpeg", alt: "Six of Cups from the Sola Busca tarot deck" },
+  {src: "assets/img/ChaosOracleDuDames.jpg", alt: "Chaos Major Arcana card from the Oracle Du Dames tarot deck, with abstract circles in red, blue, green, and yellow"}, 
+  {src: "assets/img/Sola_Busca_Cups02.jpg", alt: "Two of cups from the Sola Busca tarot deck" }, 
+  {src: "assets/img/MinchiateCups04.jpg", alt: "Four of Cups from the Minchiate tarot deck" }, 
+  {src: "assets/img/Etteilla-QueenCups.jpeg", alt: "Queen of Cups from Eitteilla's Tarot du Thot (Thoth)"}, 
+  {src: "assets/img/FiveCupsRWS.jpg", alt: "Five of Cups from the Rider-Waite-Smith tarot deck" },
+  {src: "assets/img/FrenchTarotWedding.jpeg", alt: "Page of Cups from the French Wedding tarot deck"}, 
+  {src: "assets/img/Cups02.jpg", alt: "Two of Cups from the Rider-Waite-Smith tarot deck" },
+  {src: "assets/img/Minchiate03Cups.jpeg", alt: "Three of Cups from the Minchiate tarot deck, with three illustrated cups in a triangle pattern and a sitting monkey holding their hand outstretched in the center" }
+];
 // find element for click event to be on and assign click handler
 window.onload = function(){
   // select shuffle action button
@@ -38,7 +59,8 @@ window.onload = function(){
     // Inside the loop, create an image element
     let cardImages = card.querySelector('.card_img');
     // set the src attribute of image to random image
-    cardImages.src= randomImage;
+    cardImages.src= randomImage.src;
+    cardImages.alt= randomImage.alt;
     
   }); 
   });
@@ -46,18 +68,11 @@ window.onload = function(){
 
 console.log("shuffle button");
 // create array of tarot objects
-const tarotCardSet = [
-  { title: "ace de coups", Image: ".assets/img/AceCoups1760.jpeg", alt: "Ace de Coups", cardNumberValue: "1", suit: "Cups", modal: "modal1" },
-  { title: "Ace of Cups (Rider-Waite)", img: ".assets/img/AceOfCupsFlickr.jpg", alt: "Ace of Cups (Rider-Waite)", cardNumberValue: "1", suit: "Cups", modal: "modal2" }
-  // img:
+
+
   // alt:
   // 
   // card number:
   // suit:
   // modal: 
-]
-
-
-// function to go through array
-//  target cards add images inner.HTML maybe with function 
-//  add event listener to button click
+// ]
